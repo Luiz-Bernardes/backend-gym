61b698bf6399b365bb611fb06113db00487c2bff
Luiz Henrique <luizhenbernardes@gmail.com>
2023-11-06 10:04:59 -0300

    config initializers adjusts & add spec requestes

--- config/application.rb
+++ config/application.rb
@@ -45,13 +45,5 @@ module BackendGym
         routing_specs: false
     end
 
-    #Cors
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-         origins '*'
-         resource '*', :headers => :any, :methods => [:get, :post, :options]
-       end
-    end
-
   end
 end
---
+++ spec/requests/gyms_spec.rb
@@ -0,0 +1,83 @@
+require 'rails_helper'
+
+RSpec.describe "GymsRequest", type: :request do
+  before(:each) do 
+    @gym = create(:gym)
+  end
+
+  describe "JSON Schema " do
+    it "json is expected to match json schema gym" do
+      get "/gyms/#{@gym.id}.json"
+      expect(response).to match_json_schema("gym")
+    end
+  end
+
+  describe "GET" do
+    it "index is expected response body to include json" do
+      get "/gyms.json"
+      expect(response.body).to include_json(
+        data: [
+          {
+            id: @gym.id.to_s,
+            type: "gym",
+            attributes: {
+              name: @gym.name,
+              cnpj: @gym.cnpj
+            }
+          }
+        ]
+      )
+    end
+
+    it "show is expected response body to include json" do
+      get "/gyms/#{@gym.id}.json"
+      expect(response.body).to include_json(
+        data: {
+          id: @gym.id.to_s,
+          type: "gym",
+          attributes: {
+            name: @gym.name,
+            cnpj: @gym.cnpj
+          }
+        }
+      )
+    end 
+  end
+
+  describe "POST" do
+    it "create is expected response body to include json" do
+      headers = { "ACCEPT" => "application/json" }
+      gym_params = attributes_for(:gym)
+      post "/gyms.json", params: { gym: gym_params }, headers: headers
+      expect(response.body).to include_json(
+        data: {
+          id: (@gym.id + INCREMENT).to_s,
+          type: "gym",
+          attributes: {
+            name: @gym.name,
+            cnpj: @gym.cnpj
+          }
+        }
+      )
+    end
+  end
+
+  describe "PATCH" do
+    it "update is expected response body to include json" do
+      headers = { "ACCEPT" => "application/json" }
+      @gym.name = "Nome alterado"
+      patch "/gyms/#{@gym.id}.json" , params: { gym: @gym.attributes }, headers: headers
+      expect(response.body).to include_json(
+        data: {
+          id: @gym.id.to_s,
+          type: "gym",
+          attributes: {
+            name: @gym.name,
+            cnpj: @gym.cnpj
+          }
+        }
+      )
+    end
+  end
+
+end
