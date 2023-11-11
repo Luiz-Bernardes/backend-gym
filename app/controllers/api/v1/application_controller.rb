b6be900a156103697daa1014fd4b66dff6659ccc
Luiz Henrique <luizhenbernardes@gmail.com>
2023-11-11 20:05:55 -0300

    validate json request

--- app/controllers/api/v1/application_controller.rb
+++ app/controllers/api/v1/application_controller.rb
@@ -1,2 +1,8 @@
 class Api::V1::ApplicationController < ActionController::API
+	# before_action :validate_json_request
+
+	def validate_json_request
+		return if request.headers["Accept"] =~ /json/
+		render nothing: true, status: 406
+	end
 end
